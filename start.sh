41e3b751e10919b240a6e7c37312d4c151501d8c
Sunil Kumar <66400864+iamLiquidX@users.noreply.github.com>
2021-10-18 17:48:39 +0530

    Fix ImportError libcrypto++.so

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-MEGA_VERSION=$(curl -s "https://api.github.com/repos/yzop/MegaSDK/releases/latest" | grep -oP '"tag_name": "\K(.*)(?=")' | sed -e 's/^v//')
+MEGA_VERSION=3.9.5
 pip3 -q install https://github.com/yzop/MegaSDK/releases/download/v$MEGA_VERSION/megasdk-$MEGA_VERSION-py2.py3-none-any.whl
 LIB_VERSION="${MEGA_VERSION//./0}"
 if [[ -f "/usr/local/lib/python3.9/site-packages/mega/libmega.so" ]]; then
