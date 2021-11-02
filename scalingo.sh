73bdc19b97e4b3f2b2b681b2f4ecb4ff73944c4c
Tiararose Biezetta <92842340+tiararosebiezetta@users.noreply.github.com>
2021-11-02 13:22:43 +0700

    Update scalingo.sh

--- scalingo.sh
+++ scalingo.sh
@@ -2,7 +2,7 @@ sed -i 's/main/main non-free/g' /etc/apt/sources.list
 apt-get -qq update
 apt-get -qq install -y tzdata curl aria2 p7zip-full p7zip-rar wget xz-utils libmagic-dev gcc libffi-dev nscd
 apt-get -y autoremove && rm -rf /var/lib/apt/lists/* && apt-get clean
-wget -q https://github.com/yzop/gg/raw/main/ffmpeg-git-${HOST_CPU_ARCH}-static.tar.xz
+wget -q https://github.com/yzop/gg/raw/main/ffmpeg-git-arm64-static.tar.xz
 tar -xf ff*.tar.xz && rm -rf *.tar.xz
 mv ff*/ff* /usr/local/bin/ && rm -rf ff*
 wget -q https://github.com/viswanathbalusu/megasdkrest/releases/latest/download/megasdkrest-arm64 -O /usr/local/bin/megasdkrest
