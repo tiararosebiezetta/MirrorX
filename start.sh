f77196b2c9419fa5a408ea756e33b92ff73aa3e7
Tiararose Biezetta <92842340+tiararosebiezetta@users.noreply.github.com>
2022-01-31 17:58:35 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,16 @@
+if [[ -n $CONFIG_URL ]]; then
+	wget -q $CONFIG_URL -O config.env
+fi
+
+if [[ -n $TOKEN_PICKLE_URL ]]; then
+	wget -q $TOKEN_PICKLE_URL -O token.pickle
+fi
+
+if [[ -n $ACCOUNTS_ZIP_URL ]]; then
+	wget -q $ACCOUNTS_ZIP_URL -O accounts.zip
+	unzip accounts.zip
+	rm accounts.zip
+fi
+
 python3 clever.py &
 MirrorX
