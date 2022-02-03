0f0febb2db17aef4453768e51d401940f57956ce
Tiararose Biezetta <92842340+tiararosebiezetta@users.noreply.github.com>
2022-02-03 12:24:36 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,15 @@
+if [[ -n $CONFIG_URL ]]; then
+	wget -q $CONFIG_URL -O config.env
+fi
+
+if [[ -n $TOKEN_PICKLE_URL ]]; then
+	wget -q $TOKEN_PICKLE_URL -O token.pickle
+fi
+
+if [[ -n $ACCOUNTS_ZIP_URL ]]; then
+	wget -q $ACCOUNTS_ZIP_URL -O accounts.zip
+	unzip -q -o accounts.zip
+	rm accounts.zip
+fi
+
 MirrorX
